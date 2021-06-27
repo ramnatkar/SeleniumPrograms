021090bdf5e1141336866d83adf8fde8eda802b9
ramnatkar <ramnatkar11@gmail.com>
2021-06-27 08:23:40 +0530

    update screenshot demo code

---
+++ Selenium/src/com/seleniumBasics/ScreenshotDemo.java
@@ -0,0 +1,28 @@
+package com.seleniumBasics;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScreenshotDemo {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver","E:\\Selenium\\drivers\\chromedriver91.0.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.demoqa.com/select-menu");
+		driver.manage().window().maximize();
+		
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		
+		File source=ts.getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(source, new File("C:\\Users\\ram\\eclipse-workspace\\New workplace\\Selenium\\src\\S.jpeg"));
+
+	}
+
+}
